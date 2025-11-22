62466c338bd9db125b2a5eda676c2381f1b9e9b3
Richard <elitegem84@gmail.com>
2025-11-22 02:56:11 -0500

    Imported MPAndroidChart, created history activity with test graph

---
+++ app/src/main/java/com/example/smartair/child/ChildHistoryActivity.java
@@ -0,0 +1,46 @@
+package com.example.smartair.child;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.smartair.R;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChildHistoryActivity extends AppCompatActivity {
+
+    LineChart lineChart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_child_history);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        lineChart = findViewById(R.id.lineChart);
+        List<Entry> lineEntries = new ArrayList<>();
+        lineEntries.add(new Entry(0, 4));
+        lineEntries.add(new Entry(1, 3));
+        lineEntries.add(new Entry(2, 6));
+        LineDataSet lineDataSet = new LineDataSet(lineEntries, "Data Set Label");
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineChart.invalidate(); // refresh
+
+    }
+}
