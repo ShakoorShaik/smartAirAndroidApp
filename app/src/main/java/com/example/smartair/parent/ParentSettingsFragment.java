a57732041e42af9de238d7c3756748a158565678
Shakoor Shaik <shakoorshaik7@gmail.com>
2025-11-22 21:51:45 -0500

    Added PEF and PB functionality and Asthma Zone alerts are implemented.

---
+++ app/src/main/java/com/example/smartair/parent/ParentSettingsFragment.java
@@ -0,0 +1,41 @@
+package com.example.smartair.parent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.smartair.Login;
+import com.example.smartair.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ParentSettingsFragment extends Fragment {
+
+    public ParentSettingsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_parent_settings, container, false);
+
+        Button buttonLogout = view.findViewById(R.id.buttonLogout);
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), Login.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        return view;
+    }
+}
+
